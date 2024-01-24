60a4bf3081ef17987703a0a405b0863b1b43f635
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-24 12:51:24 +0100

    Add project files.

---
+++ ThreeVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿int firstNumber;
+string someString;
+double  firstDouble;
+
+firstNumber = 10;
+someString = "Szkoła Dotneta";
+firstDouble = 12.5;
+
+Console.WriteLine($"first number is equal: {firstNumber}");
+Console.WriteLine($"first string is equal: {someString}");
+Console.WriteLine($"first number is equal: {firstDouble}");
